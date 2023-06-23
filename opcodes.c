96c42a7fc040a39c74e14782bc7fbad9f529c970
Thami911 <122853011+Thami911@users.noreply.github.com>
2023-06-23 02:24:22 +0200

    Update

--- opcodes.c
+++ opcodes.c
@@ -3,16 +3,16 @@
  * _push - pushes an element to the stack
  *
  * @dbly: head of the linked list
- * @lnum: line number
+ * @l_num: line number
  * Return: no return
  */
-void _push(stack_t **dbly, unsigned int lnum)
+void _push(stack_t **dbly, unsigned int l_num)
 {
 	int w, j;
 
 	if (!vglo.arg)
 	{
-		dprintf(2, "L%u: ", lnum);
+		dprintf(2, "L%u: ", l_num);
 		dprintf(2, "usage: push integer\n");
 		free_vglo();
 		exit(EXIT_FAILURE);
@@ -22,7 +22,7 @@ void _push(stack_t **dbly, unsigned int lnum)
 	{
 		if (!isdigit(vglo.arg[j]) && vglo.arg[j] != '-')
 		{
-			dprintf(2, "L%u: ", lnum);
+			dprintf(2, "L%u: ", l_num);
 			dprintf(2, "usage: push integer\n");
 			free_vglo();
 			exit(EXIT_FAILURE);
@@ -41,13 +41,13 @@ void _push(stack_t **dbly, unsigned int lnum)
  * _pall - prints all values on the stack
  *
  * @dbly: head of the linked list
- * @lnum: line numbers
+ * @l_num: line numbers
  * Return: no return
  */
-void _pall(stack_t **dbly, unsigned int lnum)
+void _pall(stack_t **dbly, unsigned int l_num)
 {
 	stack_t *ux;
-	(void)lnum;
+	(void)l_num;
 
 	ux = *dbly;
 
@@ -63,16 +63,16 @@ void _pall(stack_t **dbly, unsigned int lnum)
  * _pint - prints the value at the top of the stack
  *
  * @dbly: head of the linked list
- * @lnum: line number
+ * @l_num: line number
  * Return: no return
  */
-void _pint(stack_t **dbly, unsigned int lnum)
+void _pint(stack_t **dbly, unsigned int l_num)
 {
-	(void)lnum;
+	(void)l_num;
 
 	if (*dbly == NULL)
 	{
-		dprintf(2, "L%u: ", lnum);
+		dprintf(2, "L%u: ", l_num);
 		dprintf(2, "can't pint, stack empty\n");
 		free_vglo();
 		exit(EXIT_FAILURE);
@@ -85,16 +85,16 @@ void _pint(stack_t **dbly, unsigned int lnum)
  * _pop - removes the top element of the stack
  *
  * @dbly: head of the linked list
- * @lnum: line number
+ * @l_num: line number
  * Return: no return
  */
-void _pop(stack_t **dbly, unsigned int lnum)
+void _pop(stack_t **dbly, unsigned int l_num)
 {
 	stack_t *ux;
 
 	if (dbly == NULL || *dbly == NULL)
 	{
-		dprintf(2, "L%u: can't pop an empty stack\n", lnum);
+		dprintf(2, "L%u: can't pop an empty stack\n", l_num);
 		free_vglo();
 		exit(EXIT_FAILURE);
 	}
@@ -108,10 +108,10 @@ void _pop(stack_t **dbly, unsigned int lnum)
  * _swap - swaps the top two elements of the stack
  *
  * @dbly: head of the linked list
- * @lnum: line number
+ * @l_num: line number
  * Return: no return
  */
-void _swap(stack_t **dbly, unsigned int lnum)
+void _swap(stack_t **dbly, unsigned int l_num)
 {
 	int b = 0;
 	stack_t *ux = NULL;
@@ -123,7 +123,7 @@ void _swap(stack_t **dbly, unsigned int lnum)
 
 	if (b < 2)
 	{
-		dprintf(2, "L%u: can't swap, stack too short\n", lnum);
+		dprintf(2, "L%u: can't swap, stack too short\n", l_num);
 		free_vglo();
 		exit(EXIT_FAILURE);
 	}
