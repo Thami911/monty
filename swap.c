664415522ac6622eeb5c0421c7b17f966b67987b
Thami911 <122853011+Thami911@users.noreply.github.com>
2023-06-25 15:10:35 +0200

    Update

--- swap.c
+++ swap.c
@@ -23,7 +23,7 @@ void s_swap(stack_t **head, unsigned int count)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*head);
+		f_free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
 	h = *head;
